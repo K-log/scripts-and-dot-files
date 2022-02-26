818d679046b6229075957e25d5c4c89799f3e04c
k-log <noahb343@gmail.com>
2022-02-26 15:59:59 -0800

    clean up install script - use more functions

--- README.md
+++ README.md
@@ -4,3 +4,18 @@ This is a repository for all my dot-files and configs.
 Most of these configs contain code that was not written by me.
 
 Use these configs at your own risk as I configured them for my system specifically and they may not work correctly elsewhere.
+
+
+## Additional Notes
+
+
+
+Set dpi scaling for IDEA
+
+Add to Help > Edit Custom VM Options
+```text
+-Dsun.java2d.uiScale.enabled=true
+-Dide.ui.scale=1.25
+``` 
+
+https://intellij-support.jetbrains.com/hc/en-us/articles/360007994999-HiDPI-configuration
--- install.sh
+++ install.sh
@@ -2,44 +2,89 @@
 
 echo "Starting automated setup"
 
-echo "Installing git, curl, nodejs, yarn-classic, gh"
-sudo apt install git curl nodejs gh -y
-sudo npm install -g yarn
+initial_setup() {
+  echo "Starting initial package setup"
+  echo "=============================="
+  echo "Installing curl, maven, nodejs"
+  apt install curl maven nodejs -yq --print-uris
 
-echo "Installing lazygit"
-sudo add-apt-repository ppa:lazygit-team/release
-sudo apt-get update
-sudo apt-get install lazygit
+  echo "Install yarn"
+  npm install -g yarn
 
-echo "Intalling neovim"
-sudo apt install neovim python3-neovim -y
+  echo "Initial package setup complete!"
+  echo "------------------------------"
+}
 
-echo "Installing nerd fonts"
-curl -fLo "JetBrainsMono Nerd Font Complete.otf" \
-    https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/complete/JetBrainsMono%20Nerd%20Font%20Complete.otf
-sudo cp "JetBrainsMono Nerd Font Complete.otf" /usr/local/share/fonts
-
-
-
-echo "Setting up oh-my-zsh"
-sudo apt install zsh -y
-chsh -s $(which zsh)
-
-echo "Setting up vim plugins"
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-echo "Setting up config files..."
-
-ln -vs ./init.vim ~/.config/nvim/init.vim
-echo "init.vim symlinked"
-
-ln -vs ./.zshrc ~/.zshrc
-echo ".zshrc symlinked"
-
-ln -vs ./global-git ~/global-git
-echo "global-git/ symlinked"
+setup_git() {
+  echo "Starting git setup..."
+  echo "=============================="
+  echo "Installing git"
+  apt install git -yq --print-uris
 
 
-echo "Setting global git hooks path"
-git config --global core.hooksPath ~/global-git/hooks
+  echo "Installing lazygit"
+  add-apt-repository ppa:lazygit-team/release
+  apt-get update
+  apt-get install lazygit -yq --print-uris
+
+  cp -v ./global-git "$HOME/global-git"
+  echo "global-git/ copied"
+
+  echo "Setting global git hooks path"
+  git config --global core.hooksPath ~/global-git/hooks
+
+  echo "Git setup complete!"
+  echo "------------------------------"
+}
+
+
+setup_zsh() {
+    echo "Starting zsh setup..."
+    echo "=============================="
+    echo "Installing zsh and setting as default shell"
+    apt install zsh -yq --print-uris
+    chsh -s $(which zsh)
+
+    echo "Installing oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    cp -v ./.zshrc "$HOME/.zshrc"
+    echo ".zshrc copied"
+
+    echo "Install p10k zsh"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+
+    #echo "Installing nerd fonts"
+    #curl -fLo "JetBrainsMono Nerd Font Complete.otf" \
+    #    https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/complete/JetBrainsMono%20Nerd%20Font%20Complete.otf
+    #cp "JetBrainsMono Nerd Font Complete.otf" /usr/local/share/fonts
+
+    echo "Zsh setup complete!"
+    echo "------------------------------"
+}
+
+setup_neovim() {
+    echo "Starting neovim setup..."
+    echo "=============================="
+    echo "Installing neovim"
+    apt install neovim python3-neovim -yq --print-uris
+
+    mkdir "$HOME/.config/nvim/"
+    cp ./init.vim "$HOME/.config/nvim/init.vim"
+    echo "init.vim copied"
+
+    echo "Setting up vim plugins"
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
+    echo "Zsh setup complete!"
+    echo "------------------------------"
+}
+
+
+initial_setup()
+setup_git()
+setup_zsh()
+setup_neovim()
